b7a80278f7ede7e599685f38b66b437c06138e88
Pip <pip.harsveld@hva.nl>
2023-03-30 22:00:42 +0200

    Try to change svg color on input
    Doesnt work yet :(

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 // Switch to case on url
-
 var path = window.location.pathname;
 
 console.log(path);
@@ -8,8 +7,45 @@ switch (path) {
     case '/':
         console.log('Home');
         break;
-    case '/designing.html':
+
+
+    case '/design.html':
         const form = document.querySelector('form');
+        const colorInput = document.querySelector('form input[type="color"]');
+        const fileInput = document.querySelector('form input[type="file"]');
+        const image = document.createElement('img');
+        const svgShirt = document.querySelector('svg');
+
+        fileInput.addEventListener('change', (event) => {
+            console.log('file input changed');
+            const reader = new FileReader();
+
+            reader.addEventListener('load', () => {
+                console.log(reader.result);
+                image.src = reader.result;
+                image.alt = 'uploaded image';
+                image.classList.add('uploaded-image');
+                document.querySelector('.design>main>section:first-of-type').appendChild(image);
+                localStorage.setItem('image', reader.result);
+            });
+
+            if (event.target.files && event.target.files[0]) {
+                reader.readAsDataURL(event.target.files[0]);
+            }1
+        });
+
+/****************************************************************************************************************/
+/*                              WERKT NIET MAAK IK SNAP NIET WAAROM, HELP                                       */
+/****************************************************************************************************************/
+        colorInput.addEventListener('change', () => {
+            console.log('color input changed');
+
+            const colorValue = colorInput.value;
+            console.log(colorValue);
+            console.log(svgShirt.querySelector('path'));
+            svgShirt.querySelector('path').setAttribute('fill', colorValue);
+
+        });
 
         form.addEventListener('submit', function (event) {
             event.preventDefault();
@@ -19,19 +55,21 @@ switch (path) {
             const size = document.querySelector('#size').value;
             const gender = document.querySelector('input[name="gender"]:checked').value;
             const text = document.querySelector('#text').value;
-            const image = document.querySelector('#image').value;
 
             // Save form data to localStorage
             localStorage.setItem('color', color);
             localStorage.setItem('size', size);
             localStorage.setItem('gender', gender);
             localStorage.setItem('text', text);
-            localStorage.setItem('image', image);
 
             // Redirect to overview page
             window.location.href = form.action;
         });
         break;
+
+
+
+
     case '/overview.html':
         console.log('overview');
 
@@ -42,16 +80,23 @@ switch (path) {
         const size = localStorage.getItem('size');
         const gender = localStorage.getItem('gender');
         const text = localStorage.getItem('text');
-        const image = localStorage.getItem('image');
+        const savedImage = localStorage.getItem('image');
+
+        const imageElement = document.createElement('img');
+
+        imageElement.src = savedImage;
+
+        document.querySelector('.overview>main>section>section:first-of-type').appendChild(imageElement);
+
 
         // Set form data to overview page
         document.querySelector('main>section>p:first-of-type').innerHTML = 'Kleur : ' + color;
         document.querySelector('main>section>p:nth-of-type(2)').innerHTML = 'Maat : ' + size;
-        document.querySelector('main>section>p:nth-of-type(3)').innerHTML = 'Geslacht : ' + gender;
+        document.querySelector('main>section>p:nth-of-type(3)').innerHTML = 'Model : ' + gender;
 
         if (color) {
             svg.querySelector('path').setAttribute('fill', color);
-          }
+        }
 
 
         break;
