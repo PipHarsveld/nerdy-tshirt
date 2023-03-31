68b9e0b6125766b2640635accf9301795e77f52c
Pip <pip.harsveld@hva.nl>
2023-03-31 10:00:27 +0200

    Make svg color change work and begin error state
    Error state doesnt work yet

--- scripts/script.js
+++ scripts/script.js
@@ -31,47 +31,64 @@ switch (path) {
 
             if (event.target.files && event.target.files[0]) {
                 reader.readAsDataURL(event.target.files[0]);
-            }1
+            } 1
         });
 
-/****************************************************************************************************************/
-/*                              WERKT NIET MAAK IK SNAP NIET WAAROM, HELP                                       */
-/****************************************************************************************************************/
+
         colorInput.addEventListener('change', () => {
             console.log('color input changed');
 
             const colorValue = colorInput.value;
             console.log(colorValue);
             console.log(svgShirt.querySelector('path'));
-            svgShirt.querySelector('path').setAttribute('fill', colorValue);
+            svgShirt.querySelector('path').setAttribute('style', ('fill:' + colorValue));
 
         });
 
         const shirtText = document.createElement('p');
         const TextInput = document.querySelector('form input[type="text"]');
-    
+
         TextInput.addEventListener('input', (e) => {
             shirtText.textContent = e.target.value;
             document.querySelector('.design>main>section').appendChild(shirtText);
         });
 
         form.addEventListener('submit', function (event) {
+            const isValidForm = form.checkValidity();
+
             event.preventDefault();
 
-            // Get form values
-            const color = document.querySelector('#color').value;
-            const size = document.querySelector('#size').value;
-            const gender = document.querySelector('input[name="gender"]:checked').value;
-            const text = document.querySelector('#text').value;
+            if (isValidForm) {
+                console.log('form is valid');
 
-            // Save form data to localStorage
-            localStorage.setItem('color', color);
-            localStorage.setItem('size', size);
-            localStorage.setItem('gender', gender);
-            localStorage.setItem('text', text);
+                // Get form values
+                const color = document.querySelector('#color').value;
+                const size = document.querySelector('#size').value;
+                const gender = document.querySelector('input[name="gender"]:checked').value;
+                const text = document.querySelector('#text').value;
 
-            // Redirect to overview page
-            window.location.href = form.action;
+                // Save form data to localStorage
+                localStorage.setItem('color', color);
+                localStorage.setItem('size', size);
+                localStorage.setItem('gender', gender);
+                localStorage.setItem('text', text);
+
+                // Redirect to overview page
+                window.location.href = form.action;
+
+            } else {
+                console.log('form is not valid');
+
+                /***************************/
+                /* Error state werkt niet? */
+                /***************************/
+                const modelInput = document.querySelector('form');
+
+                modelInput.classList.add('error');
+            }
+
+
+
         });
         break;
 
@@ -87,14 +104,16 @@ switch (path) {
         const color = localStorage.getItem('color');
         const size = localStorage.getItem('size');
         const gender = localStorage.getItem('gender');
-        const text = localStorage.getItem('text');
+        const savedText = localStorage.getItem('text');
         const savedImage = localStorage.getItem('image');
-
         const imageElement = document.createElement('img');
+        const textElement = document.createElement('p');
 
         imageElement.src = savedImage;
+        textElement.innerText = savedText;
 
         document.querySelector('.overview>main>section>section:first-of-type').appendChild(imageElement);
+        document.querySelector('.overview>main>section>section:first-of-type').appendChild(textElement);
 
 
         // Set form data to overview page
@@ -105,6 +124,9 @@ switch (path) {
         if (color) {
             svg.querySelector('path').setAttribute('fill', color);
         }
+
+
+
 
 
         break;
